0c43b9faed96f924341b417335c0000a5d690655
rjsilvaoficial <rjsilvaoficial@gmail.com>
2022-02-26 13:02:17 -0300

    Delete Seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -46,6 +46,32 @@ namespace SalesWebMVC.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            //Para casos nullables sempre inclua o atb.Value como parametro em um método que o usará
+            var seller = _sellerService.FindById(id.Value);
+            if(seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
 
